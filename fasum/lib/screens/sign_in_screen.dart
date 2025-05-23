909109ac1d6fad32f740560d7341247f2a1ad748
JordiWillyam <jordiwillyamadikusuma_2226240138@mhs.mdp.ac.id>
2025-05-23 20:23:32 +0700

    Update sign_in_screen.dart

--- fasum/lib/screens/sign_in_screen.dart
+++ fasum/lib/screens/sign_in_screen.dart
@@ -53,7 +53,8 @@ class SignInScreenState extends State<SignInScreen> {
                       password: _passwordController.text,
                     );
                     Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => const HomeScreen()),
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
                     );
                   } catch (error) {
                     setState(() {
@@ -73,7 +74,8 @@ class SignInScreenState extends State<SignInScreen> {
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const SignUpScreen()),
+                    MaterialPageRoute(
+                        builder: (context) => const SignUpScreen()),
                   );
                 },
                 child: const Text('Don\'t have an account? Sign up'),
@@ -85,3 +87,166 @@ class SignInScreenState extends State<SignInScreen> {
     );
   }
 }
+
+// tampilan lebih bagus
+// class SignInScreen extends StatefulWidget {
+//   const SignInScreen({super.key});
+//   @override
+//   SignInScreenState createState() => SignInScreenState();
+// }
+
+// class SignInScreenState extends State<SignInScreen> {
+//   final _emailController = TextEditingController();
+//   final _passwordController = TextEditingController();
+//   final _formKey = GlobalKey<FormState>();
+//   bool _isLoading = false;
+//   bool _isPasswordVisible = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Sign In')),
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: SingleChildScrollView(
+//             child: Form(
+//               key: _formKey,
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   TextFormField(
+//                     controller: _emailController,
+//                     keyboardType: TextInputType.emailAddress,
+//                     decoration: const InputDecoration(
+//                       labelText: 'Email',
+//                       border: OutlineInputBorder(),
+//                       prefixIcon: Icon(Icons.email),
+//                     ),
+//                     validator: (value) {
+//                       if (value == null ||
+//                           value.isEmpty ||
+//                           !_isValidEmail(value)) {
+//                         return 'Please enter a valid email';
+//                       }
+//                       return null;
+//                     },
+//                   ),
+//                   const SizedBox(height: 16.0),
+//                   TextFormField(
+//                     controller: _passwordController,
+//                     obscureText: !_isPasswordVisible,
+//                     decoration: InputDecoration(
+//                       labelText: 'Password',
+//                       border: const OutlineInputBorder(),
+//                       prefixIcon: const Icon(Icons.lock),
+//                       suffixIcon: IconButton(
+//                         icon: Icon(
+//                           _isPasswordVisible
+//                               ? Icons.visibility
+//                               : Icons.visibility_off,
+//                         ),
+//                         onPressed: () {
+//                           setState(() {
+//                             _isPasswordVisible = !_isPasswordVisible;
+//                           });
+//                         },
+//                       ),
+//                     ),
+//                     validator: (value) {
+//                       if (value == null || value.isEmpty) {
+//                         return 'Please enter your password';
+//                       }
+//                       return null;
+//                     },
+//                   ),
+//                   const SizedBox(height: 16.0),
+//                   _isLoading
+//                       ? const CircularProgressIndicator()
+//                       : ElevatedButton(
+//                           onPressed: _signIn,
+//                           child: const Text('Sign In'),
+//                         ),
+//                   const SizedBox(height: 16.0),
+//                   RichText(
+//                     text: TextSpan(
+//                       style: const TextStyle(
+//                         fontSize: 16.0,
+//                         color: Colors.black,
+//                       ),
+//                       children: [
+//                         const TextSpan(text: "Don't have an account?"),
+//                         TextSpan(
+//                           text: "Sign Up",
+//                           style: const TextStyle(
+//                             color: Colors.blue,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                           recognizer: TapGestureRecognizer()
+//                             ..onTap = () {
+//                               Navigator.push(
+//                                 context,
+//                                 MaterialPageRoute(
+//                                   builder: (context) => const SignUpScreen(),
+//                                 ),
+//                               );
+//                             },
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   void _signIn() async {
+//     if (!_formKey.currentState!.validate()) {
+//       return;
+//     }
+//     final email = _emailController.text.trim();
+//     final password = _passwordController.text;
+//     setState(() => _isLoading = true);
+//     try {
+//       await FirebaseAuth.instance.signInWithEmailAndPassword(
+//         email: email,
+//         password: password,
+//       );
+//       Navigator.of(context).pushReplacement(
+//         MaterialPageRoute(builder: (context) => const HomeScreen()),
+//       );
+//     } on FirebaseAuthException catch (error) {
+//       _showSnackBar(_getAuthErrorMessage(error.code));
+//     } catch (error) {
+//       _showSnackBar('An error occurred: $error');
+//     } finally {
+//       setState(() => _isLoading = false);
+//     }
+//   }
+
+//   void _showSnackBar(String message) {
+//     ScaffoldMessenger.of(
+//       context,
+//     ).showSnackBar(SnackBar(content: Text(message)));
+//   }
+
+//   bool _isValidEmail(String email) {
+//     String emailRegex =
+//         r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zAZ0-9-]+)*$";
+//     return RegExp(emailRegex).hasMatch(email);
+//   }
+
+//   String _getAuthErrorMessage(String code) {
+//     switch (code) {
+//       case 'user-not-found':
+//         return 'No user found with that email';
+//       case 'wrong-password':
+//         return 'Wrong password. Please try again.';
+//       default:
+//         return 'An error occurred. Please try again.';
+//     }
+//   }
+// }
